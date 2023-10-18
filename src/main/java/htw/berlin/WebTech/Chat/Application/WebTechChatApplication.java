5d016434dc1a682cf75e0481d61767206dd22286
endidii <thieuandyhuy@gmail.com>
2023-10-18 16:02:49 +0200

    first commit, projekt erstellt

---
+++ src/main/java/htw/berlin/WebTech/Chat/Application/WebTechChatApplication.java
@@ -0,0 +1,13 @@
+package htw.berlin.WebTech.Chat.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTechChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTechChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw/berlin/WebTech/Chat/Application/WebTechChatApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.WebTech.Chat.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTechChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
