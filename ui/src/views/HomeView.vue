7a264d9597519286c98774be5f230befc3dc83b2
endidii <thieuandyhuy@gmail.com>
2023-11-15 15:57:33 +0100

    vue hello world

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import MyHelloWorld from "@/views/MyHelloWorld.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <MyHelloWorld />
   </main>
 </template>
---
+++ ui/src/views/MyHelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>Hello World! 1</div>
+</template>
+
+<style scoped>
+
+</style>
