cc2fa0c3c81ff222ba4bfa6d1b53f9b56f18c299
endidii <thieuandyhuy@gmail.com>
2023-11-15 16:18:34 +0100

    vue project neu installiert

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# ui
+# frontend
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
-import MyHelloWorld from "@/views/MyHelloWorld.vue";
 </script>
 
 <template>
   <main>
-    <MyHelloWorld />
+    <TheWelcome />
   </main>
 </template>
