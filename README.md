090d7549b4cb23286f8594cab60e86166ef61da8
endidii <thieuandyhuy@gmail.com>
2023-11-22 01:44:30 +0100

    README edited, frontend welcome page

--- README.md
+++ README.md
@@ -2,11 +2,20 @@
 **ChatTalk** wurde im Zuge des Moduls "Web-Technologien" im Wi23/24 von Andy Thieu entwickelt.
 
 ## Installation
-
+**Frontend**
+```bash
+cd .\frontend\
+```
 ```bash
 npm install
 ```
-
 ```bash
 npm run dev
+```
+**Backend**
+```bash
+docker volume create web-chat-app
+```
+```bash
+docker run --name web-chat-app -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -d -v web-chat-app postgres
 ```
--- frontend/src/views/HelloWorldView.vue
+++ frontend/src/views/HelloWorldView.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-  <div>Hello world</div>
+  <div class="logo-div">
+    <img src="src/assets/chat.png" class="logo_img">
+    <p class="app-name">ChatTalk</p>
+  </div>
 </template>
 
-<style scoped>
-
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+.logo_img{
+  height: 100px;
+  margin-right: 10px;
+}
+body{
+  background-color: white;
+}
+.app-name{
+  color: black;
+  font-family: "Roboto", sans-serif;
+  font-weight: bold;
+  font-size: 40px;
+}
+.logo-div{
+  display: flex;
+  align-items: center;
+}
 </style>
