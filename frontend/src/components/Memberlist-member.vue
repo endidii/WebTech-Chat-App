6399a0de74efd4a95963747ca3bf9312c0531b35
endidii <thieuandyhuy@gmail.com>
2023-11-29 18:20:27 +0100

    changed to composition api

--- frontend/src/components/Memberlist-member.vue
+++ frontend/src/components/Memberlist-member.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="member" v-for="user in users" :key="user.id">
+  <div class="member" v-for="user in users.values()" :key="user.id">
     <img class="user-img" src="../assets/cat1.jpeg" alt="user-img" />
     <p class="username">{{user.username}}</p>
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import axios from 'axios'
+import { type Ref, ref} from "vue"
 
-export default {
-  data() {
-    return {
-      users: []
-    }
-  },
-  mounted() {
-    axios
-      .get('http://localhost:8080/channels/abca7/users')
-      .then((response) => {
-        this.users = response.data
-      })
-  }
+type User = {
+  id: string;
+  username: string;
 }
+
+let users: Ref<User[]> = ref([]);
+
+axios
+    .get('http://localhost:8080/channels/abca7/users')
+    .then((response) => {
+      users.value = response.data
+    })
+
 </script>
 
 <style scoped>
