85b68e7fbed52995e932969d207056965760cfaa
endidii <thieuandyhuy@gmail.com>
2023-12-01 13:27:52 +0100

    changed every component to composition api

--- frontend/src/components/Textchannel-button.vue
+++ frontend/src/components/Textchannel-button.vue
@@ -2,23 +2,23 @@
   <button class="channel-button" v-for="channel in channels" :key="channel.id"><span class="chat-tag"># {{channel.name}}</span></button>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import axios from 'axios'
+import { type Ref, ref} from "vue"
 
-export default {
-  data() {
-    return {
-      channels: []
-    }
-  },
-  mounted() {
-    axios
-      .get('http://localhost:8080/users/0b816/channels')
-      .then((response) => {
-        this.channels = response.data
-      })
-  }
+type Channel = {
+  id: string;
+  name: string;
+  description: string;
 }
+
+let channels: Ref<Channel[]> = ref([]);
+
+axios
+    .get('http://localhost:8080/users/0b816/channels')
+    .then((response) => {
+      channels.value = response.data
+    })
 </script>
 
 <style scoped>
