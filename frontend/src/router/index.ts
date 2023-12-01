c5630b06411ccdbc1903d6cd1f1f2404246bdcb5
endidii <thieuandyhuy@gmail.com>
2023-12-01 13:08:25 +0100

    changed router, deployed on vercel

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: 'https://blendr-five.vercel.app',
       name: 'home',
       component: HomeView
     },
