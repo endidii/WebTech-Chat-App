b9d9583272ef8e751f8b5b60f8f50d35f5e49e04
endidii <thieuandyhuy@gmail.com>
2023-12-01 13:10:45 +0100

    revert change

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: 'https://blendr-five.vercel.app',
+      path: '/',
       name: 'home',
       component: HomeView
     },
