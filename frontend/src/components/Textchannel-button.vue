0498daa91cb7f479629a13909ce0474e2b93c618
endidii <thieuandyhuy@gmail.com>
2023-12-10 21:41:41 +0100

    adding and showing textchannel working

--- frontend/src/components/Textchannel-button.vue
+++ frontend/src/components/Textchannel-button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="channel-button" v-for="channel in channels" :key="channel.id"><span class="chat-tag"># {{channel.name}}</span></button>
+  <button class="channel-button" v-for="channel in channelObjects" :key="channelObjects.id"><span class="chat-tag"># {{channel.name}}</span></button>
 </template>
 
 <script setup lang="ts">
@@ -12,7 +12,7 @@ type Channel = {
   description: string;
 }
 
-let channels: Ref<Channel[]> = ref([]);
+let channelObjects: Ref<Channel[]> = ref([]);
 
 const props = defineProps({
   userData: Object
@@ -24,8 +24,28 @@ function getChannels(){
   axios
       .get(`http://localhost:8080/users/${props.userData.id}/channels`)
       .then((response) => {
-        channels.value = response.data
-        console.log(response.data)
+        let userId = props.userData.id;
+        let json = response.data;
+        json.forEach(item => {
+          if (typeof item === 'object' && item.users) {
+            item.users.forEach(user => {
+              if (user.id === userId) {
+                // Add the entire top-level channel object
+                if (item.name) {
+                  channelObjects.value.push(item);
+                }
+                // Iterate through the user's textchannels
+                user.textchannels.forEach(channel => {
+                  // Check if the item in textchannels is a channel object and add it
+                  if (typeof channel === 'object' && channel.name) {
+                    channelObjects.value.push(channel);
+                  }
+                });
+              }
+            });
+          }
+        });
+        console.log(channelObjects.value);
       })
 }
 </script>
