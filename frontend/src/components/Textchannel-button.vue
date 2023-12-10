46c83e2122011df3c2f4334afac862554e504b2b
endidii <thieuandyhuy@gmail.com>
2023-12-10 22:11:52 +0100

    fixed linter errors

--- frontend/src/components/Textchannel-button.vue
+++ frontend/src/components/Textchannel-button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="channel-button" v-for="channel in channelObjects" :key="channelObjects.id"><span class="chat-tag"># {{channel.name}}</span></button>
+  <button class="channel-button" v-for="channel in channelObjects" :key="channel.id"><span class="chat-tag"># {{channel.name}}</span></button>
 </template>
 
 <script setup lang="ts">
@@ -9,8 +9,20 @@ import {onMounted, type Ref, ref} from "vue"
 type Channel = {
   id: string;
   name: string;
-  description: string;
-}
+  description?: string;
+  users: User[];
+  messages: any[];
+};
+
+type User = {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+  messages: any[];
+  textchannels: (string | Channel)[];
+  directchannels: any[];
+};
 
 let channelObjects: Ref<Channel[]> = ref([]);
 
@@ -22,13 +34,13 @@ onMounted(() => {
 })
 function getChannels(){
   axios
-      .get(`http://localhost:8080/users/${props.userData.id}/channels`)
+      .get(`http://localhost:8080/users/${props.userData?.id}/channels`)
       .then((response) => {
-        let userId = props.userData.id;
+        let userId = props.userData?.id;
         let json = response.data;
-        json.forEach(item => {
+        json.forEach((item:Channel) => {
           if (typeof item === 'object' && item.users) {
-            item.users.forEach(user => {
+            item.users.forEach((user:User) => {
               if (user.id === userId) {
                 // Add the entire top-level channel object
                 if (item.name) {
