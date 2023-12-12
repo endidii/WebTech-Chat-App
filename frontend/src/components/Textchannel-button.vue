e906d01bbbd15aec594a56fa56155b040e81a8a5
endidii <thieuandyhuy@gmail.com>
2023-12-12 18:37:00 +0100

    simplified fetching channels of user and displaying them

--- frontend/src/components/Textchannel-button.vue
+++ frontend/src/components/Textchannel-button.vue
@@ -1,108 +1,85 @@
 <template>
   <button
-      @click="buttonClicked(channel)"
-      class="channel-button"
-      v-for="channel in channelsForUser"
-      :class="{ active: activeChannelId === channel.id }"
-      :key="channel.id">
-    <span class="chat-tag"># {{channel.name}}</span>
+    @click="buttonClicked(channel)"
+    class="channel-button"
+    v-for="channel in channelsForUser"
+    :class="{ active: activeChannelId === channel.id }"
+    :key="channel.id"
+  >
+    <span class="chat-tag"># {{ channel.name }}</span>
   </button>
 </template>
 
 <script setup lang="ts">
 import axios from 'axios'
-import {onMounted, type Ref, ref} from "vue"
-
-type Message = {
-  id: string;
-  content: string;
-  timestamp: string;
-  user: User;
-  textchannel: Channel;
-};
+import { onMounted, type Ref, ref } from 'vue'
 
 type Channel = {
-  id: string;
-  name: string;
-  description: string;
-  users: User[];
-  messages: any[];
-};
+  id: string
+  name: string
+  description: string
+  users: User[]
+  messages: any[]
+}
 
 type User = {
-  id: string;
-  username: string;
-  email: string;
-  password: string;
-  messages: any[];
-  textchannels: (Channel)[];
-  directchannels: any[];
-};
+  id: string
+  username: string
+  email: string
+  password: string
+  messages: any[]
+  textchannels: Channel[]
+  directchannels: any[]
+}
 
+const emit = defineEmits(['channelButtonClicked'])
 
-const emit = defineEmits(["channelButtonClicked"])
-
-let channelsForUser: Ref<Channel[]> = ref([]);
+let channelsForUser: Ref<Channel[]> = ref([])
 
 const props = defineProps({
   userData: Object,
   activeChannelId: String
-});
-onMounted(() => {
-  getChannels()
 })
-function buttonClicked(channel: Channel){
-  emit("channelButtonClicked", channel)
-}
-function getChannels(){
-  axios
-      .get(`http://localhost:8080/users/${props.userData?.id}/channels`)
-      .then((response) => {
-        let userId = props.userData?.id;
-        let jsonResponse = response.data;
-        jsonResponse.forEach((item: Channel | string) => {
-          if (typeof item === 'object' && 'users' in item) {
-            const isUserInTopLevel = item.users.some(user =>
-                (typeof user === 'object' && user.id === userId) || user === userId
-            );
-
-            if (isUserInTopLevel && !channelsForUser.value.some(channel => channel.id === item.id)) {
-              channelsForUser.value.push(item);
-            }
-
-            item.users.forEach((user: User | string) => {
-              if (typeof user === 'object' && user.id === userId) {
-                user.textchannels.forEach((textchannel: Channel | string) => {
-                  if (typeof textchannel === 'string') {
-                    // Handle string IDs in textchannels
-                    const channelObject = jsonResponse.find(channel =>
-                        typeof channel === 'object' && channel.id === textchannel
-                    ) as Channel | undefined;
-
-                    if (channelObject && !channelsForUser.value.some(channel => channel.id === channelObject.id)) {
-                      channelsForUser.value.push(channelObject);
-                    }
-                  } else if (!channelsForUser.value.some(channel => channel.id === textchannel.id)) {
-                    // Handle channel objects in textchannels
-                    channelsForUser.value.push(textchannel);
-                  }
-                });
-              }
-            });
-          }
-        });
-
-        console.log(channelsForUser.value);
-      })
+onMounted(() => {
+  axios.get(`http://localhost:8080/users/${props.userData?.id}/channels`)
+    .then(response => {
+      let channelIds = response.data.map((item: Channel | string) => {
+        if (typeof item === 'object') {
+          // If item is an object, return the id property of the item
+          return item.id;
+        } else {
+          // If item is not an object (thus a string), return the item itself
+          return item;
+        }
+      });
+      // Map each channelId to a fetch promise
+      const fetchPromises = channelIds.map(async (channelId: string) => {
+        try {
+          const channelResponse = await axios.get(`http://localhost:8080/channels/${channelId}`);
+          return channelResponse.data;
+        } catch (error) {
+          console.error("Error fetching channel ${channelId}:", error);
+          return null; // Return null in case of error
+        }
+      });
+      // Use Promise.all to wait for all promises to resolve
+      Promise.all(fetchPromises).then(channelsData => {
+        // Filter out any null values and update channelsForUser
+        channelsForUser.value = channelsData.filter(channel => channel !== null);
+      });
+    });
+});
+function buttonClicked(channel: Channel) {
+  emit('channelButtonClicked', channel)
 }
 </script>
 
 <style scoped>
-.channel-button:hover{
-  background-color: #006EDB;
+.channel-button:hover {
+  background-color: #006edb;
   cursor: pointer;
 }
 .active {
-  background-color: #006EDB;
+  background-color: #006edb;
 }
 </style>
