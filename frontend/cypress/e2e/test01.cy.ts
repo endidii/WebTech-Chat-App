25d380a850988464fcc93a9e34e0e7c0d19d056c
endidii <thieuandyhuy@gmail.com>
2024-01-05 19:52:40 +0100

    first cypress test

--- frontend/cypress/e2e/test01.cy.ts
+++ frontend/cypress/e2e/test01.cy.ts
@@ -1,5 +1,12 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('renders the login page', () => {
+    cy.visit('http://localhost:5173/');
+
+    cy.get('.blendr-name')
+      .should("exist")
+      .should("contain", "blendr");
+
+    cy.get('.sign-up-div')
+      .should("exist")
   })
 })
